735ffe44171b323ec3dd4031f98bfa279206d829
Alikhan1337. <alihanaripjan@gmail.com>
2024-11-13 08:57:08 +0500

    Add files via upload

---
+++ for.js
@@ -0,0 +1,40 @@
+/*
+for(let a=1;a<10;a++){
+  if(a==5){
+    alert(a);
+    break;
+  }
+  alert(a);
+}
+*/
+/*
+for(let i = 10; i >= 0; i--){
+  if(i==5 || i==6){
+      continue;
+  }
+  console.log(i);
+}
+*/
+/*
+let num = +prompt("Enter any num");
+let sum = 0;
+for (let a = 1; a<=num; a++){
+  sum=sum+a;
+  console.log("Sum right now->" +sum);
+}
+alert(num + "Your answer is:" + sum)
+*/
+
+let num = +prompt("Enter number:")
+console.log("3ke and 5ke bolinetin san:");
+for (let i = 1; i <= num; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i); jane
+  if (i % 2 === 0) {
+    console.log(i + "sany 3ke and 5ke qalduksiz bolinedi jup san-> break");
+  break;
+}
+  }
+    }
+//home work ^
+/*          |       */
